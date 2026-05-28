ae33d7bde59b0ffd0ec93b8320f11a05713c5aa0
abdurrahim-bayraktar <abdurrahimbayraktar@gmail.com>
2026-05-28 17:19:24 +0300

    regression hybrid

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,12 +2,13 @@
 
 from .baseline_lstm import BaselineLSTM, AttentionLSTM, SentimentAttention, DualBranchLSTM
 from .hybrid_attention_lstm import HybridAttentionLSTM
+from .hybrid_regression_lstm import HybridRegressionLSTM
 from .losses import CombinedLoss
 from .simple_lstm import SimpleTrendLSTM, MediumTrendLSTM
 
 __all__ = [
     "BaselineLSTM", "AttentionLSTM", "SentimentAttention", "DualBranchLSTM",
-    "HybridAttentionLSTM",
+    "HybridAttentionLSTM", "HybridRegressionLSTM",
     "CombinedLoss", "SimpleTrendLSTM", "MediumTrendLSTM"
 ]
 
